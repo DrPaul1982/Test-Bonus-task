6538bd19846d1b964be981502bee586a155a473d
DrPaul1982 <avtoklavir@ukr.net>
2024-09-03 14:00:53 +0300

    Update BonusServiceTest.java
    Test name changed

--- src/test/java/org/example/app/service/BonusServiceTest.java
+++ src/test/java/org/example/app/service/BonusServiceTest.java
@@ -1,6 +1,7 @@
 package org.example.app.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,19 +16,22 @@ class BonusServiceTest {
     }
 
     @Test
-    void getRes() {
+    @DisplayName("Test sells volume is expected result.")
+    void getRes_WithCorrectValue() {
         String res = service.getRes(0.1);
         assertEquals("0,01", res);
     }
 
     @Test
+    @DisplayName("Test sells volume is unexpected result.")
     void getRes_WithInvalidValue() {
         String res = service.getRes(0.0);
         assertEquals("Incorrect value!", res);
     }
 
     @Test
-    void calcBonus() {
+    @DisplayName("Test calculating of bonus is expected result.")
+    void calcBonus_TenProcents() {
        double result = service.calcBonus(100);
        assertEquals(10.0, result);
 
